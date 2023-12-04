6a6583774c2f7754d2e2e31759da417e9d874a34
EsaAryaMahardika <esamahardika53@gmail.com>
2023-12-04 12:23:06 +0700

    Firebase

--- android/app/src/main/java/com/coba/MainApplication.java
+++ android/app/src/main/java/com/coba/MainApplication.java
@@ -1,4 +1,4 @@
-package com.coba;
+package com.woco;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
--- ios/cobaTests/cobaTests.m
+++ ios/cobaTests/cobaTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface cobaTests : XCTestCase
+@interface wocoTests : XCTestCase
 
 @end
 
-@implementation cobaTests
+@implementation wocoTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
